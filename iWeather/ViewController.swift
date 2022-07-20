560c62449fd221b3d0d0c0b09b0e89dd2e5c083a
Jignasa Joshi <56236307+jig-josh@users.noreply.github.com>
2022-07-20 12:08:57 -0400

    Complete IBAction coding.

--- iWeather/ViewController.swift
+++ iWeather/ViewController.swift
@@ -9,6 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func cityWeatherRequest(_ sender: Any) {
+        if let cityName = self.cityNameTextFld.text, cityName != "" {
+            self.getTempIndicator.isHidden = false
+            self.getTempIndicator.startAnimating()
+            WebServiceManager.sharedInstance.callWeatherAPI(forCity: cityName) { (success, message, data) in
+                DispatchQueue.main.async {
+                    //Once API call happened to stop the activity indicator regardless of succuss or failure
+                    self.getTempIndicator.stopAnimating()
+                    self.getTempIndicator.isHidden = true
+                }
+                if success, let responseData = data {
+                    DispatchQueue.main.async {
+                        self.tempView.isHidden = false
+                        self.currentTempLbl.text = "\(responseData.temp)"
+                        self.minTempLbl.text = "\(responseData.temp_min)"
+                        self.maxTempLbl.text = "\(responseData.temp_max)"
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        self.tempView.isHidden = true
+                        self.showAlert(with: "Data not available now. Please try after some time")
+                    }
+                }
+            }
+            
+        } else {
+            showAlert(with: "Please enter a city name")
+        }
+    }
+    
+    func showAlert(with message: String){
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        alert.addAction(alertAction)
+        self.present(alert, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
